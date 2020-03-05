2c1ff049c9901a85f13de853f9c7d896a86e0e37
daniel <danielhp95@gmail.com>
2020-03-05 14:36:24 +0000

    bumped version 0.3 -> 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gym_connect4',
-      version='0.3',
+      version='0.4',
       description='OpenAI gym environment for Connect 4',
       url='https://github.com/Danielhp95/gym-connect4',
       author='Sarios',
